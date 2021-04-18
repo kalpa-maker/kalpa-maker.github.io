5b89af9ff4fa992f93f7c15d34e53417e74f843e
kalpa-maker <71049592+kalpa-maker@users.noreply.github.com>
2021-04-19 01:04:37 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,24 +23,12 @@ function blog_posts() {
     // keeping it static, can be fetched from a blog dynamically as well
     let posts = [
         {
-            url: 'https://www.nagekar.com/2017/02/trip-to-bramhatal-uttarakhand.html',
-            title: 'Trek To Bramhatal (Uttarakhand)',
+            url: 'https://kalpa.neocities.org/SpringCloud1.html',
+            title: 'SpringCloud-微服务架构笔记',
         },
         {
-            url: 'https://www.nagekar.com/2017/08/privacy.html',
-            title: 'Privacy - How I Converted',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-            title: 'Jagriti Yatra 2017',
-        },
-        {
-            url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-            title: 'ELI5 - How HTTPS Works',
+            url: 'https://kalpa.neocities.org/SpringCloud_Eureka.html',
+            title: 'SpringCloud Eureka&Consul-服务发现笔记',
         },
     ];
 
